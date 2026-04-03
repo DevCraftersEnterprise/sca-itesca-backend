a13ea193a34d9a341445097254cf7c49a840e94e
Cris1222 <105466361+Cris1222@users.noreply.github.com>
2026-04-02 20:26:33 -0700

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,16 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
   });
-
+  // --- CONFIGURACIÓN DE SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('API ITESCA - Instructores')
+    .setDescription('Documentación de las APIs para el sistema de constancias')
+    .setVersion('1.0')
+    .addBearerAuth() // Por si usas JWT
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   // 2. Usar el puerto que asigne Render o el 3000 por defecto
   const port = process.env.PORT || 3000;
   await app.listen(port);
