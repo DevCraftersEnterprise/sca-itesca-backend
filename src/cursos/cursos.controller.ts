447dd65a465e0ecc1e95786d77d35f85f12bf153
Cris1222 <105466361+Cris1222@users.noreply.github.com>
2026-04-05 18:48:09 -0700

    Obtain details about the course, attendance, and enrolled employees v2

--- src/cursos/cursos.controller.ts
+++ src/cursos/cursos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, UnauthorizedException, ParseIntPipe } from '@nestjs/common';
 import { CursosService } from './cursos.service';
 import { CreateCursoDto } from './dto/create-curso.dto';
 import { UpdateCursoDto } from './dto/update-curso.dto';
@@ -24,13 +24,7 @@ export class CursosController {
     const adminId = req.user.id;
     return this.cursosService.create(createCursoDto, adminId);
   }
-  // 2. Listar todos los cursos (Solo ADMIN)
-  @Get()
-  @Roles(Role.ADMIN)
-  @UseGuards(AuthGuard('jwt'), RolesGuard)
-  findAll() {
-    return this.cursosService.findAll();
-  }
+
   // 3. Inscribirse a un curso (Solo EMPLEADO)
   @Post('inscribir')
   @Roles(Role.ADMIN)
@@ -42,55 +36,16 @@ export class CursosController {
     const usuarioId = req.user.id;
     return this.cursosService.inscribir(cursoId, usuarioId);
   }
-  // 4. Ver mis inscripciones (Solo EMPLEADO)
-  @Get('mis-inscripciones')
-  @Roles(Role.EMPLEADO)
-  @UseGuards(AuthGuard('jwt'), RolesGuard)
-  async misInscripciones(@Req() req: any) {
-    const usuarioId = req.user.id; 
-    if (!usuarioId) {
-       throw new UnauthorizedException('No se encontró el ID del usuario en el token');
-    }
-
-    return this.cursosService.obtenerMisInscripciones(usuarioId);
-  }
-  // 5. Listar cursos disponibles para inscribirse (Solo EMPLEADO)
-  @Get('disponibles')
-  @Roles(Role.EMPLEADO)
-  @UseGuards(AuthGuard('jwt'))
-  async listarDisponibles(@Req() req: any) {
-    return this.cursosService.listarDisponibles(req.user.id);
-  }
-  // 6. Listar cursos que imparto (Solo INSTRUCTOR)
-  @Get('mis-cursos')
-  @Roles(Role.INSTRUCTOR)
-  @UseGuards(AuthGuard('jwt'))
-  async listarMisCursos(@Req() req: any) {
-    return this.cursosService.listarMisCursos(req.user.id);
-  }
-
-  // 7. Ver detalle de un curso al que estoy inscrito (Solo EMPLEADO)
-  @Get('mis-constancias')
-  @UseGuards(AuthGuard('jwt'))
-  async verMisConstancias(@Req() req: any) {
-    return this.cursosService.obtenerMisConstancias(req.user.id);
-  }
-  // 8. Listar mis inscripciones activas (Solo EMPLEADO)
-  @Get('mis-inscripciones-activas')
-  @UseGuards(AuthGuard('jwt'))
-  async listarMisInscripciones(@Req() req: any) {
-    return this.cursosService.listarMisInscripciones(req.user.id);
-  }
-  // 9. Ver detalle de un curso al que estoy inscrito (Solo EMPLEADO)
-  @Get('mi-curso/:id')
-  @UseGuards(AuthGuard('jwt'))
-  async verDetalleCurso(@Param('id') id: string, @Req() req: any) {
-    return this.cursosService.verDetalleCurso(+id, req.user.id);
-  }
-  // 10. Ver detalle de un curso (Solo ADMIN)
+  // 1. Para el botón "Ver curso" -> /cursos/:id
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cursosService.findOne(+id);
+  async getDetalleCurso(@Param('id', ParseIntPipe) id: number) {
+    return this.cursosService.findOne(id);
+  }
+
+  // 2. Para el botón "Asistencia" -> /cursos/:id/asistencia
+  @Get(':id/asistencia')
+  async getAsistenciasCurso(@Param('id', ParseIntPipe) id: number) {
+    return this.cursosService.findAsistencias(id);
   }
   // 11. Actualizar curso (Solo ADMIN)
   @Patch(':id')
